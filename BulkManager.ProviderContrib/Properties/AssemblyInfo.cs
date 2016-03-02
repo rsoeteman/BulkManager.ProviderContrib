8aa98f0de85d29e219a443deb062330c9e0bd708
Richard Soeteman <richard@soetemansoftware.nl>
2016-03-02 14:23:34 +0100

    Version and readme update

--- BulkManager.ProviderContrib/Properties/AssemblyInfo.cs
+++ BulkManager.ProviderContrib/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BulkManager.ProviderContrib")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Additional bulkactions for Bulkmanager")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BulkManager.ProviderContrib")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCompany("Soetemans Software")]
+[assembly: AssemblyProduct("BulkManager")]
+[assembly: AssemblyCopyright("Copyright © Soetemans Software 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
